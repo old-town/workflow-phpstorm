7114c3b2407c3c4ecbeeef73f10d66d19f9bfa9b
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-11-29 11:54:23 +0300

    sm-94 Реализация автокомплитера подставляющего названия классов для функций

--- src/ru/oldtown/idea/workflowplugin/config/workflow/xml/XmlHelper.java
+++ src/ru/oldtown/idea/workflowplugin/config/workflow/xml/XmlHelper.java
@@ -16,34 +16,11 @@ import java.util.Collection;
  */
 public class XmlHelper {
 
-    /**
-     * @param tag
-     * @param attributeNames
-     * @return
-     */
-    public static PsiElementPattern.Capture<PsiElement> getTagAttributePattern(String tag, String... attributeNames) {
-        return XmlPatterns
-                .psiElement()
-                .inside(XmlPatterns
-                                .xmlAttributeValue()
-                                .inside(XmlPatterns
-                                                .xmlAttribute()
-                                                .withName(StandardPatterns.string().oneOfIgnoreCase(attributeNames))
-                                                .withParent(XmlPatterns
-                                                                .xmlTag()
-                                                                .withName(tag)
-                                                )
-                                )
-                ).inFile(getXmlFilePattern());
-    }
 
-    /**
-     * @return
-     */
     public static PsiFilePattern.Capture<PsiFile> getXmlFilePattern() {
         return XmlPatterns.psiFile()
                 .withName(XmlPatterns
-                                .string().endsWith(".xml")
+                        .string().endsWith(".xml")
                 );
     }
 
@@ -51,45 +28,28 @@ public class XmlHelper {
         return XmlPatterns
                 .psiElement(XmlTokenType.XML_DATA_CHARACTERS)
                 .withParent(XmlPatterns
-                                .xmlText()
-                                .withParent(
+                        .xmlText()
+                        .withParent(
 
-                                        XmlPatterns
-                                                .xmlTag()
-                                                .withName("arg").withChild(
-                                                XmlPatterns.xmlAttribute("name").withValue(
-                                                        XmlPatterns.string().equalTo("class.name")
-                                                )
+                                XmlPatterns
+                                        .xmlTag()
+                                        .withName("arg").withChild(
+                                        XmlPatterns.xmlAttribute("name").withValue(
+                                                XmlPatterns.string().equalTo("class.name")
                                         )
-
-
                                 )
+
+
+                        )
                 ).inside(XmlPatterns
-                                .psiElement(XmlTag.class)
-                                .withName("function").withChild(
-                                        XmlPatterns.xmlAttribute("type").withValue(
-                                                XmlPatterns.string().equalTo("class")
-                                        )
+                        .psiElement(XmlTag.class)
+                        .withName("function").withChild(
+                                XmlPatterns.xmlAttribute("type").withValue(
+                                        XmlPatterns.string().equalTo("class")
                                 )
+                        )
                 ).inFile(getXmlFilePattern());
     }
-
-    /**
-     * @param insideTagName
-     * @return
-     */
-    public static PsiElementPattern.Capture<XmlTag> getInsideTagPattern(String insideTagName) {
-        return XmlPatterns.psiElement(XmlTag.class).withName(insideTagName);
-    }
-
-    /**
-     * @param insideTagName
-     * @return
-     */
-    public static PsiElementPattern.Capture<XmlTag> getInsideTagPattern(String... insideTagName) {
-        return XmlPatterns.psiElement(XmlTag.class).withName(XmlPatterns.string().oneOf(insideTagName));
-    }
-
 
 
     @NotNull
--- src/ru/oldtown/idea/workflowplugin/util/completion/PhpNamespaceCompletionProvider.java
+++ src/ru/oldtown/idea/workflowplugin/util/completion/PhpNamespaceCompletionProvider.java
@@ -4,66 +4,111 @@ import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.completion.InsertHandler;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
+import com.jetbrains.php.PhpIcons;
 import com.jetbrains.php.PhpIndex;
-import com.jetbrains.php.completion.PhpCompletionUtil;
-import com.jetbrains.php.completion.PhpLookupElement;
 import com.jetbrains.php.completion.insert.PhpNamespaceInsertHandler;
-import com.jetbrains.php.completion.insert.PhpReferenceInsertHandler;
-import com.jetbrains.php.lang.psi.elements.PhpClass;
-import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
+import com.jetbrains.php.lang.PhpLangUtil;
 import org.jetbrains.annotations.NotNull;
-import ru.oldtown.idea.workflowplugin.util.PhpElementsUtil;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 
 public class PhpNamespaceCompletionProvider extends CompletionProvider<CompletionParameters> {
 
-    private final boolean withInterface;
-    private boolean withLeadingBackslash = false;
-
-    public PhpNamespaceCompletionProvider() {
-        this(false);
-    }
-
-    public PhpNamespaceCompletionProvider(boolean withInterface) {
-        this.withInterface = withInterface;
-    }
-
-    public PhpNamespaceCompletionProvider withTrimLeadBackslash(boolean withLeadingBackslash) {
-        this.withLeadingBackslash = withLeadingBackslash;
-        return this;
-    }
 
     public void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, final @NotNull CompletionResultSet resultSet) {
 
         PsiElement psiElement = parameters.getOriginalPosition();
 
-        addNamespaceCompletion(parameters, resultSet, psiElement, withInterface, withLeadingBackslash);
+        addNamespaceCompletion(parameters, resultSet, psiElement);
 
     }
-    public static void addNamespaceCompletion(CompletionParameters parameters, final CompletionResultSet resultSet, PsiElement psiElement, boolean withInterface) {
-        addNamespaceCompletion(parameters, resultSet, psiElement, withInterface, false);
-    }
 
-    public static void addNamespaceCompletion(CompletionParameters parameters, final CompletionResultSet resultSet, PsiElement psiElement, boolean withInterface, boolean withLeadBackslash) {
+    public static void addNamespaceCompletion(CompletionParameters parameters, final CompletionResultSet resultSet, PsiElement psiElement) {
         PsiElement position = parameters.getPosition().getOriginalElement();
         PhpIndex index = PhpIndex.getInstance(position.getProject());
+
 //
         int cursorOffset = parameters.getOffset();
         int cursorOffsetClean = cursorOffset - psiElement.getTextOffset();
 
         String content = psiElement.getText();
         if(cursorOffsetClean < 1) {
-            content = "";
+            content = "\\";
         }
 
+        Integer slashLastIndex = content.lastIndexOf("\\");
 
-        PhpCompletionUtil.addSubNamespaces(content, resultSet, index, PhpNamespaceInsertHandler.getInstance());
+        if (slashLastIndex.equals(-1)) {
+            return;
+        }
 
+        String namespace = "";
+        if (slashLastIndex.equals(0)) {
+            namespace = "";
+        }
 
+        if (slashLastIndex > 0) {
+            namespace = content.substring(0, content.lastIndexOf("\\"));
+        }
+
+        addSubNamespaces(namespace, resultSet, index, PhpNamespaceInsertHandler.getInstance());
 
     }
 
+    public static void addSubNamespaces(@NotNull String namespaceName, @NotNull CompletionResultSet result, @NotNull PhpIndex phpIndex, @Nullable InsertHandler<LookupElement> insertHandler) {
+        Collection phpNamespaces = getAllChildNamespaceNames(phpIndex, PhpLangUtil.toFQN(namespaceName));
+        Iterator i$ = phpNamespaces.iterator();
 
+        while(i$.hasNext()) {
+            String namespace = (String)i$.next();
+            String  matchNamespace = namespace;
+            if (!matchNamespace.startsWith("\\")) {
+                matchNamespace = "\\" + matchNamespace;
+            }
+
+
+            if(!"___PHPSTORM_HELPERS".equals(namespace) && result.getPrefixMatcher().prefixMatches(matchNamespace)) {
+                String shortName = PhpLangUtil.toShortName(namespace);
+                //String partialNamespaceName = StringUtil.trimStart(namespace, PhpLangUtil.toPresentableFQN(namespaceName));
+                LookupElementBuilder element = LookupElementBuilder.create(matchNamespace).withPresentableText(shortName).withIcon(PhpIcons.NAMESPACE).withInsertHandler(insertHandler);
+                if(!PhpLangUtil.equalsClassNames(shortName, namespace)) {
+                    element = element.withTailText("(" + namespace + ")", true);
+                }
+
+                result.addElement(element);
+            }
+        }
+
+    }
+
+    @NotNull
+    private static Collection<String> getAllChildNamespaceNames(@NotNull PhpIndex phpIndex, @NotNull String parent) {
+        ArrayList values = new ArrayList();
+        PriorityQueue toProcess = new PriorityQueue();
+        toProcess.add(parent);
+
+        while(!toProcess.isEmpty()) {
+            String next = (String)toProcess.poll();
+            Collection children = phpIndex.getChildNamespacesByParentName(next);
+            Iterator i$ = children.iterator();
+
+            while(i$.hasNext()) {
+                String child = (String)i$.next();
+                String childQualifiedName = next + child;
+                toProcess.add(childQualifiedName + "\\");
+                values.add(PhpLangUtil.toPresentableFQN(childQualifiedName));
+            }
+        }
+
+        return values;
+    }
 }
